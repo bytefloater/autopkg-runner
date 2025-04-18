c4efee494e7640479ac6a3c90adafeadde794e88
Ellis Dickinson <67054721+ellisdickinson46@users.noreply.github.com>
2025-04-18 23:47:15 +0100

    Bump version

--- __info__.py
+++ __info__.py
@@ -1,7 +1,7 @@
 # Application Info
 APP_NAME = "autopkg_runner"
 FRIENDLY_APP_NAME = "AutoPkg Runner"
-APP_VERSION_STR = "0.6.0"
+APP_VERSION_STR = "2.0.0"
 APP_CHANNEL = "Production"
 CONFIG_FILE = "config.json"
 
