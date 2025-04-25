c5a1e112ac84664d416c0cc1ee272413a0f80c17
Ellis Dickinson <67054721+ellisdickinson46@users.noreply.github.com>
2025-04-25 20:24:41 +0100

    Add testing logic
    Added ability to run stages individually for testing and command line interface for overriding basic settings

--- libs/orchestrator.py
+++ libs/orchestrator.py
@@ -4,28 +4,47 @@ from libs.stage import Stage
 from libs.config import PipelineConfig
 from stages import (
     EnvironmentCheck, TrustVerification, MountRepository, RunAutoPkg,
-    GenerateReport, GarbageCollector, NotifyOnCompletion
+    GarbageCollector, GenerateReport, NotifyOnCompletion
 )
 
 class Orchestrator:
+    STAGE_CLASSES: list[Stage] = [
+        EnvironmentCheck,
+        MountRepository,
+        TrustVerification,
+        RunAutoPkg,
+        GarbageCollector,
+        GenerateReport,
+        NotifyOnCompletion
+    ]
+
     def __init__(self, config: PipelineConfig, logger: Logger):
         # Shared “scratch‐space” between stages
         self.config = config
         self.ctx: dict = {}
         self.logger: Logger = logger
 
+    def configure_stages(self, override_stage_name):
+        if override_stage_name:
+            override_stage_class: list[Stage] = next(
+                ([cls] for cls in self.STAGE_CLASSES if cls.__name__ == override_stage_name),
+                []
+            )
+
         # Instantiate each stage with the same configuration
         self.stages: list[Stage] = [
-            EnvironmentCheck(config, self.ctx, self.logger),
-            TrustVerification(config, self.ctx, self.logger),
-            MountRepository(config, self.ctx, self.logger),
-            RunAutoPkg(config, self.ctx, self.logger),
-            GenerateReport(config, self.ctx, self.logger),
-            GarbageCollector(config, self.ctx, self.logger),
-            NotifyOnCompletion(config, self.ctx, self.logger)
+            cls(self.config, self.ctx, self.logger)
+            for cls in (override_stage_class if 'override_stage_class' in locals() else self.STAGE_CLASSES)
         ]
 
+        enabled_stage_names = [cls.__class__.__name__ for cls in self.stages]
+        self.logger.info(f"Configured stages: {enabled_stage_names}")
+
     def execute(self):
+        if not hasattr(self, "stages"):
+            self.logger.error("Stages not configured")
+            return
+
         completed = []
         try:
             for stage in self.stages:
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import argparse
 import inspect
+import platform
 import sys
+import textwrap
 from logbook import Logger, Processor, StreamHandler
 
 from __info__ import LOGGER_NAME, FRIENDLY_APP_NAME, APP_VERSION_STR, APP_CHANNEL
@@ -21,7 +24,7 @@ def _inject_classprefix(record):
                 return
         frame = frame.f_back
 
-    # fallback if nothing found
+    # Fallback if nothing found
     record.extra['classprefix'] = ''
 
 def setup_logger(log_level) -> Logger:
@@ -44,16 +47,52 @@ def setup_logger(log_level) -> Logger:
 
 
 if __name__ == "__main__":
-    config = load_config("config.json")
+    stages_available = {cls.__name__: cls for cls in Orchestrator.STAGE_CLASSES}
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description=textwrap.dedent("""\
+        AutoPkg Runner
+        --------------
+        A wrapper to run AutoPkg on a remote SMB repository and peform post-run
+        clean-up tasks
+        """)
+    )
+    pipeline_args = parser.add_argument_group("workflow options")
+    pipeline_args.add_argument(
+        "-c", "--config",
+        default="config.json",
+        help="Path to your JSON config file (default: config.json)"
+    )
+    testing_args = parser.add_argument_group("stage testing options")
+    testing_args.add_argument(
+        "-s", "--stage",
+        choices=stages_available.keys(),
+        metavar="STAGE",
+        help=(
+            "Run one stage for testing\n"
+            f"Stages: {', '.join(stages_available.keys())}"
+        )
+    )
+
+    # Application setup
+    args = parser.parse_args()
+    config = load_config(args.config)
     logger = setup_logger(config.log_level)
+
+    # Platform Check
+    if platform.system() != "Darwin":
+        logger.error("This utility only works on macOS!")
+        sys.exit(126)
 
     # Log application information
     logger.info(f"Starting {FRIENDLY_APP_NAME}")
     logger.info(f"  App Version: {APP_VERSION_STR}")
     logger.info(f"  App Channel: {APP_CHANNEL}")
 
+    # Setup workflow orchestrator
     orchestrator = Orchestrator(
         config=config,
         logger=logger
     )
+    orchestrator.configure_stages(args.stage)
     orchestrator.execute()
