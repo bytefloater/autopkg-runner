fc6d8cfe2818b6f90a1a01ea9eed73bef012932a
Ellis Dickinson <67054721+ellisdickinson46@users.noreply.github.com>
2026-06-01 11:45:35 +0100

    Update __info__.py
    * Removed reference to legacy configuration file

--- __info__.py
+++ __info__.py
@@ -1,9 +1,8 @@
 # Application Info
-APP_NAME = "autopkg_runner"
+APP_NAME = "autopkg-runner"
 FRIENDLY_APP_NAME = "AutoPkg Runner"
 APP_VERSION_STR = "3.0.0"
 APP_CHANNEL = "Production"
-CONFIG_FILE = "config.json"
 
 # Logger
 LOGGER_NAME = APP_NAME
